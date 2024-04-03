a6da52f2906180e57c58c8f89b7d7c9a919f678e
livinglemming <mikulas.rezab@gmail.com>
2024-04-03 16:44:06 +0200

    Added tooltips/lore Example: Profession: [farmer]

--- src/main/java/com/livinglemming/Events/RightClickEventListener.java
+++ src/main/java/com/livinglemming/Events/RightClickEventListener.java
@@ -1,33 +1,36 @@
 package com.livinglemming.Events;
 
-import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
-import net.fabricmc.fabric.api.event.player.UseItemCallback;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.item.SpawnEggItem;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.nbt.NbtString;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
 
 public class RightClickEventListener {
     public static void registerRightClickEvent() {
         UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
-            if (player.isSneaking() && entity instanceof VillagerEntity) {
-                VillagerEntity villager = (VillagerEntity) entity;
+            if (player.isSneaking() && entity instanceof VillagerEntity villager) {
                 NbtCompound nbt = new NbtCompound();
                 villager.writeCustomDataToNbt(nbt);
 
                 Item spawnEgg = SpawnEggItem.forEntity(villager.getType());
                 if (spawnEgg != null) {
                     ItemStack spawnEggStack = new ItemStack(spawnEgg);
+
                     NbtCompound nbtCompound = new NbtCompound();
+                    NbtCompound textCompound = new NbtCompound();
+                    NbtList tooltipList = new NbtList();
+
                     nbtCompound.put("EntityTag", nbt);
+                    tooltipList.add(NbtString.of("{\"text\":\"Profession: [" + villager.getVillagerData().getProfession().toString() +  "]\",\"color\":\"gray\",\"italic\":false}"));
+                    textCompound.put("Lore", tooltipList);
+                    nbtCompound.put("display", textCompound);
+
                     spawnEggStack.setNbt(nbtCompound);
                     player.giveItemStack(spawnEggStack);
                 }
